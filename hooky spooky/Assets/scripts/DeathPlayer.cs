6e8535b85f591630e6fabbcba98b9c7b11da0fd0
VuurGirl <vuurgirl.karlijn.snijders@gmail.com>
2023-03-14 11:24:36 +0100

    death player + pickup

--- hooky spooky/Assets/scripts/DeathPlayer.cs
+++ hooky spooky/Assets/scripts/DeathPlayer.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class DeathPlayer : MonoBehaviour
 {
+    public GameObject Player;
+    public CapsuleCollider enemyColider;
+    public GameObject DeathScreen;
+    public GameObject Enemy;
+    public GameObject playerShooter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DeathScreen.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag== "enemy")
+        {
+            Player.SetActive(false);
+            playerShooter.SetActive(false);
+            Enemy.SetActive(false);
+            DeathScreen.SetActive(true);
+        }
     }
 }
---
+++ hooky spooky/Assets/scripts/PickUp.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public GameObject Bullet;
+    public Collider player;
+    public GameObject Ammo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Bullet.SetActive(false);
+            transform.SetParent(Ammo.transform);
+           
+        }
+    }
+}
